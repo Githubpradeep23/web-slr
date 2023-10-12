946e666c02f2d795f1d54cfe3f7eb493bdf7a1c7
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-12 15:50:56 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,8 @@
 
-// import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-// import Footer from '@/components/Footer'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <body >
-{/*         <Navbar /> */}
-     <p>welcome slr</p>
-{/*         <Footer /> */}
+        <Navbar />
+         {children}
+        <Footer />
       </body>
     </html>
   )
