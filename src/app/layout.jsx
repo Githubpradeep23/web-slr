2023e0d3143907c54d52daa395dc60fc39501b45
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-12 15:49:06 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,9 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <body > */}
+        <body >
 {/*         <Navbar /> */}
-        <h1>welcome slr</h1>
+     <p>welcome slr</p>
 {/*         <Footer /> */}
       </body>
     </html>
