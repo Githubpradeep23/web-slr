cfdba193ba300e4941be15c4b486a91b41e73c5c
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-12 17:12:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  // Other Next.js configuration options
-
-  // Enable automatic static generation for 404 pages
-  // This will generate static pages for any paths not defined in your routes
-  // and allow for custom error pages to be served by Vercel
+    images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
 };
