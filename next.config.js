99dbc317b905032094f76c5669ef98c8fe056669
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-14 16:00:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  }
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // }
+  basePath: "",
+  output: "standalone",
+  experimental: {
+  appDir: true,
+},
 };
 
 module.exports = nextConfig;
