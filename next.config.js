97556e0e827abd5b61929174bad73149bf3e35db
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-10-15 19:18:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@
  
   images: {
   formats: ['image/jpeg', 'image/png'], // Add any other desired formats
-  domains: ['example.com'], // Add domains from which images will be served
   loader: 'default', // Use 'default' or 'imgix' loader, depending on your preference
   path: '/_next/image',
   quality: 90,
