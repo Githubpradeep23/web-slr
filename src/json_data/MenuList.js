de2a880ab0598b66d0ea473612bfb56b15905513
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-11-30 08:44:45 +0530

    Add files via upload

--- src/json_data/MenuList.js
+++ src/json_data/MenuList.js
@@ -1,6 +1,6 @@
 export const MenuList = [
   { name: "Home", link: "/", icon: "" },
-  { name: "About", link: "/", icon: "" },
+  { name: "About", link: "/pages/about", icon: "" },
   {
     name: "Services",
     link: "#",
